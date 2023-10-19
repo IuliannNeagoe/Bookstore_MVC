e32b9f118e32d1b9830473da1ea88db2168da9d7
neagoeIulian <neagoe.iulian98@gmail.com>
2023-10-19 13:01:41 +0300

    Add: browse image + displaying it on the Upsert page

--- BookstoreWeb/Areas/Customer/Controllers/ProductController.cs
+++ BookstoreWeb/Areas/Customer/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Bookstore.Models.Models;
 using Bookstore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net;
 
 namespace BookstoreWeb.Areas.Customer.Controllers
 {
@@ -10,9 +11,11 @@ namespace BookstoreWeb.Areas.Customer.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         #region Index
@@ -57,26 +60,55 @@ namespace BookstoreWeb.Areas.Customer.Controllers
         }
 
         [HttpPost]
-        public IActionResult Upsert(ProductViewModel productViewModel)
+        public IActionResult Upsert(ProductViewModel productViewModel, IFormFile myFile)
         {
             if (!ModelState.IsValid)
             {
                 //we need to populate the CategoryList again
                 productViewModel.CategoryList = _unitOfWork.CategoryRepository.GetAll()
-                    .Select(c => new SelectListItem() 
-                    { 
-                        Text = c.Name, 
-                        Value=c.Id.ToString()
+                    .Select(c => new SelectListItem()
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
                     });
 
                 return View(productViewModel);
             }
 
-            _unitOfWork.ProductRepository.Add(productViewModel.Product);
+            if (myFile != null)
+            {
+                //first check if a file is already uploaded, in that case we need to delete the old one
+                if (!string.IsNullOrEmpty(productViewModel.Product.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productViewModel.Product.ImageUrl.TrimStart('\\'));
+                    if(System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                if(UploadImage(myFile, out string? fileName))
+                {
+                    productViewModel.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+            }
+            
+            
+            if(productViewModel.Product.Id == 0) 
+            {
+                _unitOfWork.ProductRepository.Add(productViewModel.Product);
+            }
+            else
+            {
+                _unitOfWork.ProductRepository.Update(productViewModel.Product);
+            }
+
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully!";
             return RedirectToAction("Index", "Product");
         }
+
+
         #endregion
 
         #region Delete
@@ -93,7 +125,7 @@ namespace BookstoreWeb.Areas.Customer.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            if(id == null) return NotFound();
+            if (id == null) return NotFound();
 
             Product? productToDelete = _unitOfWork.ProductRepository.Get(p => p.Id == id);
             _unitOfWork.ProductRepository.Remove(productToDelete);
@@ -103,5 +135,29 @@ namespace BookstoreWeb.Areas.Customer.Controllers
         }
 
         #endregion
+
+
+
+        private bool UploadImage(IFormFile myFile, out string? fileName)
+        {
+            string productFolderPath = _webHostEnvironment.WebRootPath + @"\images\product";
+
+            try
+            {
+                fileName = Guid.NewGuid().ToString() + Path.GetExtension(myFile.FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(productFolderPath, fileName), FileMode.Create))
+                {
+                    myFile.CopyTo(fileStream);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                fileName = null;
+                return false;
+            }
+        }
     }
 }
