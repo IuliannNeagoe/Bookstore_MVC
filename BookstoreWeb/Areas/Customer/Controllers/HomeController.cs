4a5543ff6641673022c53e7babb26d5506e4c684
neagoeIulian <neagoe.iulian98@gmail.com>
2023-10-20 16:12:59 +0300

    Improvement - Add Home Page UI + Details View

--- BookstoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookstoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Bookstore.DataAccess.Repositories.Interfaces;
 using Bookstore.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,16 +9,20 @@ namespace BookstoreWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
+        #region Index
         public IActionResult Index()
         {
-            return View();
+            var productsFromDb = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category");
+            return View(productsFromDb);
         }
+        #endregion
 
         public IActionResult Privacy()
         {
@@ -28,6 +33,18 @@ namespace BookstoreWeb.Areas.Customer.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? myId)
+        {
+            if (myId == 0 || myId == null) return NotFound();
+            
+            var productFromDb = _unitOfWork.ProductRepository.Get(p => p.Id == myId, includeProperties:"Category");
+
+            if(productFromDb == null) return NotFound();
+
+            return View(productFromDb);
         }
     }
 }
