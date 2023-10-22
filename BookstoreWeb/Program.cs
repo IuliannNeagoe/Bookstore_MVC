f1c6ed23d10d5b146e134fef6f623bf21a233dcf
neagoeIulian <neagoe.iulian98@gmail.com>
2023-10-22 16:42:33 +0300

    BUGFIX - Default paths for login/logout/accessdenied not working

--- BookstoreWeb/Program.cs
+++ BookstoreWeb/Program.cs
@@ -18,6 +18,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
+//this has to be added after the "AddIdentity"
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 //because of adding roles to the AddIdentity method, we need to add an implementation of EmailSender
