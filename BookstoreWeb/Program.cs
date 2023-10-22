26ae3f9e262a0ddbf8f971603dd8ef0b058e89e8
neagoeIulian <neagoe.iulian98@gmail.com>
2023-10-22 13:12:30 +0300

    Change - let users login without having to verify the mail

--- BookstoreWeb/Program.cs
+++ BookstoreWeb/Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
