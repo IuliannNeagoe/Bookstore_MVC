88571a1ef77503f19edb9161886765622306b800
neagoeIulian <neagoe.iulian98@gmail.com>
2024-01-09 14:11:27 +0200

    Add - posibility to add items to a shoppingCart from the details page

--- BookstoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookstoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using Bookstore.DataAccess.Repositories.Interfaces;
 using Bookstore.Models.Models;
+using Bookstore.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BookstoreWeb.Areas.Customer.Controllers
 {
@@ -39,12 +42,43 @@ namespace BookstoreWeb.Areas.Customer.Controllers
         public IActionResult Details(int? myId)
         {
             if (myId == 0 || myId == null) return NotFound();
-            
-            var productFromDb = _unitOfWork.ProductRepository.Get(p => p.Id == myId, includeProperties:"Category");
 
-            if(productFromDb == null) return NotFound();
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.ProductRepository.Get(p => p.Id == myId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = myId.Value
+            };
+         
 
-            return View(productFromDb);
+            if(cart.Product == null) return NotFound();
+
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize] //we dont need to specify any role, the user just has to be logged in, no matter the role
+        public IActionResult Details(ShoppingCart? cart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            cart.ApplicationUserId = userId;
+
+
+            var existingCartFromDb = _unitOfWork.ShoppingCartRepository.Get(c => c.ApplicationUserId == userId && c.ProductId == cart.ProductId);
+            if (existingCartFromDb != null)
+            {
+                //user cart already existing for the product, so increment the count
+                existingCartFromDb.Count += cart.Count;
+                _unitOfWork.ShoppingCartRepository.Update(existingCartFromDb);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCartRepository.Add(cart);
+            }
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
