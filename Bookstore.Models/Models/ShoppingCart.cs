3d1a968ebb777bcd84e207abe4d32967d37a7003
Iulian-Gabriel Neagoe <neagoe.iulian98@gmail.com>
2024-03-22 15:30:27 +0200

    Improvement - shoppingCart Add/Remove items

--- Bookstore.Models/Models/ShoppingCart.cs
+++ Bookstore.Models/Models/ShoppingCart.cs
@@ -14,13 +14,16 @@ namespace Bookstore.Models.Models
         public Product Product { get; set; }
 
 
-        [Range(1,1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
 
         public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; } //not linked to DB, just for display purposes
 
     }
 }
--- BookstoreWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BookstoreWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -24,15 +24,59 @@ namespace BookstoreWeb.Areas.Customer.Controllers
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             var items = _unitOfWork.ShoppingCartRepository.GetAll(c => c.ApplicationUserId == userId, nameof(Product));
-            ShoppingCartViewModel = new()
+            ShoppingCartViewModel = new ShoppingCartViewModel
             {
                 ListedItems = items,
-                OrderTotal = items.Sum(i => MathHelper.GetPriceBasedOnQuantity(i) * i.Count)
             };
+
+            foreach (var item in ShoppingCartViewModel.ListedItems)
+            {
+                item.Price = MathHelper.GetPriceBasedOnQuantity(item);
+                ShoppingCartViewModel.OrderTotal += item.Price * item.Count;
+            }
 
             return View(ShoppingCartViewModel);
         }
 
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCartRepository.Get(s => s.Id == cartId);
+            if (cartFromDb == null) return NotFound();
+
+            cartFromDb.Count++;
+            _unitOfWork.ShoppingCartRepository.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCartRepository.Get(s => s.Id == cartId);
+            if (cartFromDb == null) return NotFound();
+
+            if (cartFromDb.Count <= 1)
+            {
+                //remove from cart
+                _unitOfWork.ShoppingCartRepository.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count--;
+                _unitOfWork.ShoppingCartRepository.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCartRepository.Get(s => s.Id == cartId);
+            if (cartFromDb == null) return NotFound();
+            _unitOfWork.ShoppingCartRepository.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
