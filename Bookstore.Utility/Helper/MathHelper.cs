3c80bc216e730a39706bc924483e8d7127842bb3
Iulian-Gabriel Neagoe <neagoe.iulian98@gmail.com>
2024-03-22 14:49:11 +0200

    Add - Helper class for basic calculus operations

---
+++ Bookstore.Utility/Helper/MathHelper.cs
@@ -0,0 +1,17 @@
+﻿using Bookstore.Models.Models;
+
+namespace Bookstore.Utility.Helper
+{
+    public static class MathHelper
+    {
+        public static double GetPriceBasedOnQuantity(ShoppingCart item)
+        {
+            return item.Count switch
+            {
+                <= 50 => item.Product.Price,
+                <= 100 => item.Product.Price50,
+                _ => item.Product.Price100
+            };
+        }
+    }
+}
